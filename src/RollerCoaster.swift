2832d49a005e1d2d8afaec901a4fa45dcc1b7e0a
Leticia Mazzo Portela <leticia.mazzo@appmoove.com.br>
2020-09-23 08:23:57 -0300

    Add version with only semaphore

--- src/RollerCoaster.swift
+++ src/RollerCoaster.swift
@@ -1,23 +1,74 @@
+//
+//  RollerCoaster.swift
+//
+//
+//  Created by Leticia Portela on 22/09/20.
+//  Copyright © 2020 Leticia Portela. All rights reserved.
+//
+
 import Foundation
 
 class RollerCoaster {
-    let totalPassengers = 25
-    let totalCars = 1
+    let totalPassengers = 5
     let carCapacity = 4
+    private var availableSeats: Int = 4
+    private var rollerCoasterWorking: Bool = false
+    private let passengerQueue = DispatchQueue(label: "passenger-queue", attributes: .concurrent)
+    private let carSemaphore = DispatchSemaphore(value: 4)
 
-    func initRollerCoaster() {
-        let rcController: RollerCoasterController = RollerCoasterController(carCapacity: carCapacity)
+    func carIsFull() -> Bool {
+        return self.availableSeats == 0
+    }
 
-        for i in 0..<totalPassengers {
-            let passenger = Passenger(passengerId: i, rcController: rcController)
-            passenger.passengerActions()
+    func passengerTriesToGetInCar(passengerId: Int) {
+//        self.passengerQueue.async {
+            if !self.carIsFull() && !self.rollerCoasterWorking {
+                print("The Passenger \(passengerId) gets in the car")
+                self.availableSeats -= 1
+                self.carSemaphore.wait()
+            }
+//        }
+    }
+
+    func carIsRunning() -> Bool {
+        if carIsFull() {
+            self.rollerCoasterWorking = true
+            return true
         }
 
-        for i in 0..<totalCars {
-            let car = Car(carId: i, rcController: rcController)
-            car.carActions()
+        return false
+    }
+
+    func passengerLeftTheCar() {
+        self.rollerCoasterWorking = false
+
+//        self.passengerQueue.async {
+            print("\nThe tour is over, the passengers can be released...\n")
+
+            while self.availableSeats != self.carCapacity {
+                print("The passengers are getting of..")
+                self.availableSeats += 1
+                self.carSemaphore.signal()
+            }
+//        }
+    }
+
+    func initRollerCoaster() {
+        while true {
+            for i in 0..<self.totalPassengers {
+                DispatchQueue(label: "passenger-queue-\(i)", attributes: .concurrent).async {
+                    self.passengerTriesToGetInCar(passengerId: i)
+                    sleep(2)
+                }
+            }
+
+            if self.carIsRunning() {
+                print("\nThe car is running...\n")
+            }
+
+            sleep(5)
+            self.passengerLeftTheCar()
+
         }
     }
 }
-
-RollerCoaster().initRollerCoaster()
